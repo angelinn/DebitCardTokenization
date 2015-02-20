00b84e342364a8a9753aaeb6f6ca630b0edc5190
Betsy <betrakiss@hotmail.com>
2015-02-20 19:36:00 +0200

    Adding User Control for login.

---
+++ DebitCardTokenization/LoginUserControl/LoginEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUserControl
+{
+    public delegate void LoginEventHandler(object sender, LoginEventArgs args);
+    public class LoginEventArgs : EventArgs
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ListBoxMarked { get; set; }
+
+        public LoginEventArgs(string un, string pw, string lbItem)
+        {
+            Username = un;
+            Password = pw;
+            ListBoxMarked = lbItem;
+        }
+    }
+}
