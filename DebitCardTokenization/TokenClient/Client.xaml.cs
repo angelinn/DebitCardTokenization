ce942644ac2c1d48fe078de9f4a2cd2b4237b9f2
Betsy <betrakiss@hotmail.com>
2015-02-20 13:24:49 +0200

    Adding TokenClient project.

---
+++ DebitCardTokenization/TokenClient/Client.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TokenClient
+{
+    public partial class Client : Window
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayMessage(object[] messageControlPair)
+        {
+            if (!Dispatcher.CheckAccess())
+                Dispatcher.Invoke(new Action<object[]>(DisplayMessage), messageControlPair);
+            else
+            {
+                TextBox box = messageControlPair[1] as TextBox;
+                string message = messageControlPair[0] as string;
+                box.Text += message;
+            }
+        }
+        private void mainWindow_Closed(object sender, EventArgs e)
+        {
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+    }
+}
