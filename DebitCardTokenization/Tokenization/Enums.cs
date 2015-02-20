36bd82957cdb554d9894570b202d1ab4d823d39b
Betsy <betrakiss@hotmail.com>
2015-02-21 00:02:17 +0200

    Adding Class Library - Tokenization.

---
+++ DebitCardTokenization/Tokenization/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokenization
+{
+    namespace Access
+    {
+        public enum AccessLevel
+        {
+            NONE = 0,
+            REGISTER = 1,
+            REQUEST = 2,
+            MASTER = 3
+        };
+    }
+
+    namespace Activities
+    {
+        public enum Activity
+        {
+            REGISTER = 12000,
+            LOGIN = 14000,
+            REGISTER_TOKEN = 15000,
+            REQUEST_CARD = 16000
+        };
+    }
+}
